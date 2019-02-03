006bd106b1dacd5d5a0dd3d192aa2a4a90052ee0
Kevin1er <vocanson.kevin1@gmail.com>
2019-02-03 19:42:53 +0100

    first commit

---
+++ SmartHome/SmartHome.Android/FirebaseAuthenticator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Firebase.Auth;
+using SmartHome;
+
+namespace SmartHome.Droid
+{
+    class FirebaseAuthenticator : IFirebaseAuthenticator
+    {
+        public async Task<string> LoginWithEmailPassword(string email, string password)
+        {
+            var user = await FirebaseAuth.Instance.
+                            SignInWithEmailAndPasswordAsync(email, password);
+            var token = await user.User.GetIdTokenAsync(false);
+            return token.Token;
+        }
+    }
+}
---
+++ SmartHome/SmartHome.Android/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Firebase;
+
+namespace SmartHome.Droid
+{
+    [Activity(Label = "SmartHome", Icon = "@mipmap/icon", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            //Firebase loading
+            FirebaseApp.InitializeApp(Application.Context);
+
+            LoadApplication(new App());
+        }
+    }
+}
